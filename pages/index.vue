c58e98f332e5ec9988bb1c01f41f6c639cfd640a
Tarun Arora <tarun.arora.030402@gmail.com>
2023-09-19 15:03:54 +0530

    Add global font, setup nuxt pages
    Signed-off-by: Tarun Arora <tarun.arora.030402@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
-<template>
-    <section>Reproduction</section>
-</template>
-  
-<script>
-export default {};
+<script setup lang="ts">
+definePageMeta({
+  middleware: (from) => {
+    // Define middleware, if needed
+  }
+})
 </script>
-  
+
+<template>
+  <div>Index</div>
+</template>
